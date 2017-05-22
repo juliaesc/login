430d7dd681b63fc1a5126c8896c8c5b97714a4d3
juliaesc <juliaescobio@gmail.com>
2017-05-22 18:16:33 -0300

    prueba

--- src/main/java/ar/com/buildingways/login/configuration/AppConfig.java
+++ src/main/java/ar/com/buildingways/login/configuration/AppConfig.java
@@ -1,6 +1,7 @@
 package ar.com.buildingways.login.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
